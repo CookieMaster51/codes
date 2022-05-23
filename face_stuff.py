d3db08ec00d0a82f250f1a98a62fb6f994aa3ba5
cookiemaster51 <jan.matis.2008@gmail.com>
2022-05-23 15:03:56 +0100

    memes

--- face_stuff.py
+++ face_stuff.py
@@ -1,3 +1,6 @@
+#THIS IS NOT MY CODEEEE CHECK OUT https://towardsdatascience.com/face-detection-in-2-minutes-using-opencv-python-90f89d7c0f81
+
+"""
 import numpy as np
 import cv2 as cv
 cap = cv.VideoCapture(0)
@@ -19,3 +22,33 @@ while True:
 # When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
+"""
+
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# To capture video from webcam. 
+cap = cv2.VideoCapture(0)
+# To use a video file as input 
+# cap = cv2.VideoCapture('filename.mp4')
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    # Display
+    cv2.imshow('img', img)
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+# Release the VideoCapture object
+cap.release()
