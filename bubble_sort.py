b9690475880c2c2484f9c7a31b85b71b7d2b3abe
cookiemaster51 <jan.matis.2008@gmail.com>
2022-05-23 12:53:34 +0100

    as

---
+++ bubble_sort.py
@@ -0,0 +1,11 @@
+unsorted = [3, 6, 32, 532, 46, 23, 3, 34, 13, 87, 4, 84]
+
+while sorted != True:
+    sorted = True
+    for i in range(0, len(unsorted) - 1):    
+        if unsorted[i] > unsorted[i + 1]:
+            unsorted[i], unsorted[i + 1] = unsorted[i + 1], unsorted[i]
+            sorted = False
+sorted = unsorted
+
+print(sorted)
